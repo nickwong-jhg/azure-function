05dc01db044a0716912daeda49ded169966f626c
nickwong-jhg <nwong3@jhg.com.au>
2025-02-06 10:04:19 +1100

    Corrected some namespaces

--- src/FunctionApp/Program.cs
+++ src/FunctionApp/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using System.Threading.Tasks;
 
-namespace Sample.FunctionApp
+namespace FunctionApp
 {
     public class Program
     {
